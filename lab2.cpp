301018f92fb8dc0c54c98049f494cf36aa728a5c
Artem Bakhar <artem.bahar@inbox.ru>
2019-09-12 21:31:27 +0300

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -38,7 +38,7 @@ system("pause");
 	cout << "summa: " << s;
 	system("pause");
 }
-*/
+
 {
 	int day, mon, year;
 	cout << "Today: \n";
@@ -48,7 +48,7 @@ system("pause");
 	enum  month{ jan=1,feb,march,apr,may,june,july,aug,sept,oct,nov,dec };
 	switch(mon)
 	{
-	case(jan):case(march):case(may):case(july):case(aug):case(oct):case(nov): 
+	case(jan):case(march):case(may):case(july):case(aug):case(oct):case(nov):
 	{
 		if (day<31)
 		{
@@ -81,7 +81,7 @@ system("pause");
 	case(dec):
 	{
 		if (day < 31)
-		{	
+		{
 			day += 1;
 			cout << day << " " << mon << " " << year;
 		}
@@ -127,4 +127,58 @@ system("pause");
 		break;
 	}
 	}
+}
+{
+int n;
+cout << "Vvod n: ";
+cin >> n;
+double summa=0;
+double fact = 1;
+for (int i = 1; i <= n; i++)
+{
+	fact = fact/i;
+	summa += fact;
+}
+cout << summa;
+}*/
+/*{
+int n;
+cin >> n;
+int i = 2;
+int del;
+int znam=2;
+while (znam <= n)
+{
+	del = 1;
+	for (; del < znam; del++)
+	{
+		cout << del << "/" << znam << "\n";
+	}
+	znam++;
+}
+}*/
+{
+	int a, b, c, vr;
+	cin >> a;
+	cin >> b;
+	c = a % b;
+	if (c == 0 && c!=1)
+	{
+		cout << b;
+	}
+	else
+	{
+	
+	while (b!=c)
+	{
+		if (b % c != 0)
+		{
+
+			vr = c;
+			c = b % c;
+			b = vr;
+		}
+		cout << c;
+	}
+}
 }
