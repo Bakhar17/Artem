6ae0b9883dae9091a8f1e793a3318daa02924414
Bakhar17 <54313730+Bakhar17@users.noreply.github.com>
2019-11-20 21:21:43 +0300

    finished

--- animal/Animal.cpp
+++ animal/Animal.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
 #include "Animal.h"
-#include <set>
 
 int main() {
-	Animal lion, tiger, elephant, fox, dog, wolf;
+	Animal lion, tiger, elephant, fox, dog, wolf,dog1;
 	lion = { "lion","savana",15 };
 	tiger = { "tiger","savana",29 };
 	elephant = { "elephant","savana",3 };
-	fox = { "fox","savana",56 };
-	dog = { "dog","savana",453 };
+	fox = { "fox","forest",56 };
+	dog = { "dog","savana",4 };
+	dog1 = { "dog","house",602 };
 	wolf = { "wolf","savana",23 };
 	std::multiset<Animal> animals;
 	animals.insert(lion);
@@ -17,11 +16,25 @@ int main() {
 	animals.insert(fox);
 	animals.insert(dog);
 	animals.insert(wolf);
+	animals.insert(dog1);
 	Animal cat;
 	for (std::multiset<Animal>::iterator i = animals.begin(); i != animals.end(); i++)
 	{
 		std::cout << *i;
 	}
-	std::cin >> cat;
-	std::cout << cat;
+	std::cout << Areal(animals,dog)<<"\n";
+	//std::multiset<Animal>::iterator i=animals.begin();
+	//std::multiset<Animal > animals3;
+	//animals3.insert(lion);
+	//animals3.insert(tiger);
+	//animals3.insert(elephant);
+	//animals3.insert(fox);
+	//animals3.insert(dog);
+	//animals3.insert(wolf);
+	//animals3.insert(dog1);
+	//for (std::multiset<Animal>::iterator i = animals3.begin();  i!=animals3.end(); i++)
+	//{
+	//	std::cout << *i << "\n";
+	//}
+	SortPlace(animals);
 }
--- animal/Animal.h
+++ animal/Animal.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <set>
 #include <string>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 class Animal
 {
 public:
@@ -14,8 +17,12 @@ public:
 	bool operator<(const Animal _Right) const {
 		return this->name < _Right.name;
 	}
+
 	std::string name, place;
 	int number;
+	friend std::string BigName(std::multiset<Animal>& b);
+	friend std::string Areal(std::multiset<Animal>& a,Animal b);
+	friend void SortPlace(std::multiset<Animal>& a);
 };
 
 Animal::Animal()
@@ -36,10 +43,48 @@ std::ostream& operator<<(std::ostream& on,const Animal& an) {
 	on<< an.name <<" "<< an.place<<" "<< an.number<<"\n";
 	return on;
 }
-bool operator<(Animal& xn, Animal& an) {
-	if (xn.name < an.name) {
-		return true;
+
+std::string BigName(std::multiset<Animal>& bc) {
+	std::set<std::pair<std::string, int>> place;
+	std::multiset<Animal>::iterator b = bc.begin();
+	while (b != bc.end()) {
+		int n = 0;
+		for (std::multiset<Animal>::iterator i = b; i != bc.end(); i++)
+	{
+			if (b->place == i->place) {
+				n++;
+		}
 	}
-	else
-		return false;
+		place.emplace(b->place, n);
+		b++;
+	}
+	b = bc.begin();
+	std::set<std::pair<std::string, int>>::iterator k = --place.end();
+	return k->first;
+}
+std::string Areal(std::multiset<Animal>& a, Animal b) {
+	std::multiset<Animal>::iterator g = a.lower_bound(b);
+	for (std::multiset<Animal>::iterator i = a.lower_bound(b); i != a.upper_bound(b); i++) {
+		if (g->number >= i->number) {
+			g = i;
+		}
+	}
+	return g->place;
+}
+void SortPlace(std::multiset<Animal>& a) {
+	std::set<std::string> places;
+	for (std::multiset<Animal>::iterator i = a.begin(); i !=a.end(); i++)
+	{
+		places.insert(i->place);
+	}
+	std::set<std::string>::iterator i = places.begin();
+	while (i != places.end()) {
+		for (std::multiset<Animal>::iterator k = a.begin(); k != a.end(); k++)
+		{
+			if (*i == k->place) {
+				std::cout << *k << "\n";
+			}
+		}
+		i++;
+	}
 }
