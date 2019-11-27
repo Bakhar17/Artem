3c3afb4bb301272af93ace16d3021e27fe959533
Bakhar17 <54313730+Bakhar17@users.noreply.github.com>
2019-11-27 21:59:29 +0300

    calc

--- 21.11.2019/21.11.2019/21112019.cpp
+++ 21.11.2019/21.11.2019/21112019.cpp
@@ -3,45 +3,10 @@
 
 int main()
 {
-	char note[100], pnote[100];
-	int i, p = 0;
-	stack s;
+	char note[100];
 	cin >> note;
-
-	for (i = 0; i < strlen(note); i++)
-	{
-		if (note[i] == '(') s.push(note[i]);
-		else if ((note[i] == '+') || (note[i] == '-') || (note[i] == '/') || (note[i] == '*'))
-		{
-			while ((!s.empty()) && (s.top_prior() > s.prior(note[i])))
-			{
-				p++;
-				pnote[p] = s.pop();
-			}
-			s.push(note[i]);
-		}
-		else if (note[i] == ')')
-		{
-			while ((!s.empty()) && (s.get_top_element() != '('))
-			{
-				p++;
-				pnote[p] = s.pop();
-			}
-			s.pop();
-		}
-		else
-		{
-			p++;
-			pnote[p] = note[i];
-		}
-	}
-	while (!s.empty())
-	{
-		p++;
-		pnote[p] = s.pop();
-	}
-	for (i = 1; i <= p; i++)
-		cout << pnote[i];
-	cout << endl;
+	string pnote = OPZ(note);
+	Calc(pnote);
+	system("pause");
 	return 0;
 }
--- 21.11.2019/21.11.2019/Header.h
+++ 21.11.2019/21.11.2019/Header.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 class stack
 {
@@ -8,9 +10,9 @@ class stack
 public:
 	int prior(char x)
 	{
-		if ((x = '*') || (x = '/')) return 2;
-		if ((x = '+') || (x = '-')) return 1;
-		if ((x = '(') || (x = ')')) return 0;
+		if ((x == '*') || (x == '/')) return 2;
+		if ((x == '+') || (x == '-')) return 1;
+		if ((x == '(') || (x == ')')) return 0;
 	}
 	stack() { top = 0; }
 	bool empty() { return top == 0; }
@@ -29,4 +31,127 @@ public:
 
 };
 
+class stackint
+{
+	int top;
+	vector<double> sum;
+public:
+	stackint() { top = 0; }
+	void push(double x)
+	{
+		top++;
+		sum.push_back(x);
+	}
+	void pop()
+	{
+		top--;
+		sum.erase(sum.end()-1);
+	}
+	double get_top_element() { return sum[top-1]; }
+};
 
+
+void Calc(const string& pnote) {
+	stackint final;
+	size_t i = 0;
+	if (pnote[0]=='-') {
+		string temp1 = "";
+		temp1 += stoi(pnote);
+		i = size(temp1);
+	}
+	for (; i < pnote.size(); i++)
+	{
+		if (pnote[i] == ' ' && (pnote[i + 1] != '+' || pnote[i + 1] != '-' || pnote[i + 1] != '*' || pnote[i + 1] != '/')) {
+			size_t t = i - 1;
+			while (pnote[t] >= '1' && t != 0 && pnote[t] <= '9') {
+				t--;
+			}
+			string temp1;
+			if (t != 0) {
+				t++;
+			}
+			for (; t < i; t++)
+			{
+				temp1 += pnote[t];
+			}
+			final.push(stoi(temp1));
+		}
+		if (pnote[i] == '+') {
+			double oper1 = final.get_top_element();
+			final.pop();
+			double oper2 = final.get_top_element();
+			final.pop();
+			final.push(oper2 + oper1);
+		}
+		if (pnote[i] == '-') {
+			double oper1 = final.get_top_element();
+			final.pop();
+			double oper2 = final.get_top_element();
+			final.pop();
+			final.push(oper2 - oper1);
+		}
+		if (pnote[i] == '*') {
+			double oper1 = final.get_top_element();
+			final.pop();
+			double oper2 = final.get_top_element();
+			final.pop();
+			final.push(oper2 * oper1);
+		}
+		if (pnote[i] == '/') {
+			double oper1 = final.get_top_element();
+			final.pop();
+			double oper2 = final.get_top_element();
+			final.pop();
+			final.push(oper2 / oper1 * 1.0);
+		}
+
+	}
+	std::cout << final.get_top_element() << "\n";
+}
+
+string OPZ(const char (&note)[100]) {
+	int i = 0;
+	stack s;
+	string pnote;
+	if (note[0] == '-') {
+		pnote += note[0];
+		i++;
+	}
+
+	for (; i < strlen(note); i++)
+	{
+		if (note[i] == '(') s.push(note[i]);
+		else if ((note[i] == '+') || (note[i] == '-') || (note[i] == '/') || (note[i] == '*'))
+		{
+			while ((!s.empty()) && (s.top_prior() >= s.prior(note[i])))
+			{
+				pnote += s.pop();
+			}
+			s.push(note[i]);
+		}
+		else if (note[i] == ')')
+		{
+			while ((!s.empty()) && (s.get_top_element() != '('))
+			{
+				pnote += s.pop();
+			}
+			s.pop();
+		}
+		else
+		{
+			string temp = "";
+			while (i < strlen(note) && note[i] != '+' && note[i] != '-' && note[i] != '/' && note[i] != '*' && note[i] != '(' && note[i] != ')') {
+				temp += note[i];
+				i++;
+			}
+			pnote += temp += " ";
+			i--;
+		}
+	}
+	while (!s.empty())
+	{
+		pnote += s.pop();
+	}
+	return pnote ;
+
+}
