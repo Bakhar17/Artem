6df02746f39fe1d683350a882deb0ce04895ccaa
Bakhar17 <54313730+Bakhar17@users.noreply.github.com>
2020-02-18 20:33:53 +0300

    +
    +

---
+++ sem2/17.02.2020.2/17.02.2020.2/17.02.2020.2.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n;
+	std::cout << "task number: ";
+	std::cin >> n;
+	switch (n) {
+	case(4): {
+		int a;
+		std::cout << "Put a: ";
+		std::cin >> a;
+		int x;
+		std::cout << "Put degree: ";
+		std::cin >> x;
+		__asm {
+			mov eax, 1
+			mov ebx, a
+			mov ecx, x
+			cmp ecx,0
+			je stop4
+			degree:
+			imul ebx
+			loop degree
+			mov a, eax
+			jmp end4
+			stop4:
+			mov a,1
+			jmp end4
+				end4:
+		}
+		std::cout << "a^x= " << a;
+		break; 
+	}
+	case(5): {
+		std::cout << "Put number: ";
+		int x;
+		std::cin >> x;
+		__asm {
+			mov eax,x
+			mov ebx,1
+			start:
+			cmp eax, ebx
+			jg greater
+			je right
+			jl less
+				greater:
+			shl ebx,1
+			jmp start
+				less:
+			mov x,0
+			jmp end
+				right:
+			mov x,1
+			jmp end
+			end:
+
+		}
+		if (x)std::cout << "Yes";
+		else std::cout << "No";
+		break;
+	}
+	case(3): {
+		int x;
+		std::cout << "Put the number: ";
+		std::cin >> x;
+		__asm {
+			mov eax,x
+			mov ecx,0
+				start1:
+			cmp eax,0
+			je end1
+			test eax,1
+			jz notcount
+				count:
+			add ecx, 1
+			shr eax, 1
+			jmp start1
+				notcount:
+			shr eax,1
+			jmp start1
+				end1:
+			mov x,ecx
+		}
+		std::cout << x;
+		break;
+	}
+	}
+	return 0;
+}
+
+
---
+++ sem2/17.02.2020/17.02.2020/17.02.2020.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+int main()
+{
+	int sec;
+	int hour;
+    std::cout << "Put seconds: ";
+	std::cin >> sec;
+	int minute;
+	int lsec;
+	__asm {
+		mov eax,sec
+		cdq
+		mov ebx,3600
+		div ebx
+		mov hour,eax
+		mov eax, sec
+		mov ebx,hour
+		imul ebx,3600
+		sub eax,ebx
+		mov ebx,60
+		cdq
+		div ebx
+		mov minute,eax
+		mov eax,sec
+		mov ebx,hour
+		imul ebx,3600
+		sub eax,ebx
+		mov ebx,minute
+		imul ebx,60
+		sub eax,ebx
+		mov lsec,eax
+	}
+	std::cout <<hour<<" "<<minute<<" "<<lsec;
+	/*int a, b, c;
+	int res=0;
+	std::cin >> a >> b >> c;
+	__asm {
+		
+		mov eax,a
+		mov ecx,0
+		test eax,1
+		jz stop
+		jmp next
+		stop:
+		add ecx,a
+		jmp next
+		next:
+		mov eax, b
+		test eax, 1
+		jz stop1
+		jmp next2
+		stop1:
+		add ecx, eax
+		jmp next2
+		next2:
+		mov eax, c
+		test eax, 1
+		jz stop2
+		jmp end2
+		stop2 :
+		add ecx, c
+		jmp end2
+		end2:
+		mov res,ecx
+	}
+	std::cout << "\n"<<res;*/
+	system("pause");
+	return 0;
+	
+}
+
