2adfacd9405bc750fac1f2f3d297baeb4231c209
Bakhar17 <54313730+Bakhar17@users.noreply.github.com>
2020-03-10 21:09:08 +0300

    16calc

--- sem2/09.03.20_ASM/09.03.20_ASM/09.03.20_ASM.cpp
+++ sem2/09.03.20_ASM/09.03.20_ASM/09.03.20_ASM.cpp
@@ -1,15 +1,24 @@
 ﻿#include <iostream>
-extern "C" void _stdcall Calc(int number, char* res);
+extern "C" void _stdcall IntToHex(int, char*);
+extern "C" void _stdcall Reverse(char*);
 int main()
 {
 	std::cout << "Put the number: ";
 	int n;
 	std::cin >> n;
-	char str[8] = {0};
-	Calc(25, str);
-	for (int i = 0; i < 8; i++)
-	{
-		std::cout << int(str[i]);
+	char* str = new char[8]{ '0','0','0','0','0','0','0','0' };
+	IntToHex(n,str );
+	Reverse(str);
+	int i = 0;
+	while (str[i] == '0') {
+		++i;
 	}
-    std::cout << "Hello World!\n";
+	for (i ; i < 8; i++)
+	{
+		std::cout << str[i];
+	}
+	std::cout << "\n";
+	delete[] str;
+	system("pause");
+	return 0;
 }
--- sem2/09.03.20_ASM/09.03.20_ASM/asm.asm
+++ sem2/09.03.20_ASM/09.03.20_ASM/asm.asm
@@ -1,75 +1,39 @@
-.586
+.386
 .model flat
 .data 
-mas db '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
+masd db '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
 .code
-_Calc@8 proc
-    mov eax,[esp+4]
-	mov ebx,[esp+8]
-	mov ecx,16
-calc:
-    cdq
-	div ecx
-	cmp edx,10
-	je A
-
-	cmp edx,11
-	je B
-
-	cmp edx,12
-	je C
-
-	cmp edx,13
-	je D
-
-	cmp edx,14
-	je E
-
-	cmp edx,15
-	je F
-
-	mov [ebx], edx
-	add ebx,1
-	cmp eax,0
+_Reverse@4 proc
+		mov esi,[esp+4]
+		mov ecx,0
+		mov edx,7
+	reverse:
+		mov ah,[esi][ecx]
+		mov bh,[esi][edx]
+		mov [esi][ecx],bh
+		mov [esi][edx],ah
+		inc ecx
+		dec edx
+	cmp ecx,4
 	je finish
-
-		A:
-	mov [ebx], 'A'	
-	add ebx,1
-	cmp eax,0
-	je finish
-	jmp calc
-		B:
-	mov [ebx], 'B'	
-	add ebx,1
-	cmp eax,0
-	je finish
-	jmp calc
-		C:
-	mov [ebx], 'C'	
-	add ebx,1
-	cmp eax,0
-	je finish
-	jmp calc
-		D:
-	mov [ebx], 'D'	
-	add ebx,1
-	cmp eax,0
-	je finish
-	jmp calc
-		E:
-	mov [ebx], 'E'	
-	add ebx,1
-	cmp eax,0
-	je finish
-	jmp calc
-		F:
-	mov [ebx], 'F'	
-	add ebx,1
-	cmp eax,0
-	je finish
-	jmp calc
+	jmp reverse
 	finish:
+ret 4
+_Reverse@4 endp
+_IntToHex@8 proc
+		mov eax,[esp+4]
+		mov esi,[esp+8]
+		mov ecx,16
+calc:
+		cdq
+		div ecx
+		mov bl,masd[edx]
+		mov [esi],bl
+		cmp eax,0
+	je finish
+		add esi,1
+	jmp calc
+finish:
 ret 8
-_Calc@8 endp
+_IntToHex@8 endp
 end
