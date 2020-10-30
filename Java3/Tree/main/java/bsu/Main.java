9c9d845033b1586d5d6ebaa5bdd3568478e98478
Bakhar17 <54313730+Bakhar17@users.noreply.github.com>
2020-10-30 18:51:40 +0300

    last

--- Java3/Tree/main/java/bsu/Main.java
+++ Java3/Tree/main/java/bsu/Main.java
@@ -18,5 +18,15 @@ public class Main {
         tree.passLeftRightRoot();
         tree.passLeftRootRight();
         tree.passRootLeftRight();
+        Tree<Person> personTree = new Tree<>();
+        personTree.add(new Person(5,"Artem"));
+        personTree.add(new Person(2,"Vanya"));
+        personTree.add(new Person(6,"Gosha"));
+        personTree.add(new Person(0,"Petra"));
+        personTree.add(new Person(25,"Dima"));
+        System.out.println(personTree.binaryFind(new Person(2,"Artem")));
+        personTree.passRootLeftRight();
+        personTree.passLeftRootRight();
+        personTree.passLeftRightRoot();
     }
 }
--- Java3/Tree/main/java/bsu/Person.java
+++ Java3/Tree/main/java/bsu/Person.java
@@ -4,12 +4,22 @@ public class Person implements Comparable<Person> {
     private int level;
     private String name;
 
+    public Person(int level, String name) {
+        this.level = level;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return  name;
+    }
+
     @Override
     public int compareTo(Person o) {
-        if(level>o.level){
+        if (level > o.level) {
             return 5;
-        }else if(level==o.level){
+        } else if (level == o.level) {
             return 0;
-        }else return -3;
+        } else return -3;
     }
 }
